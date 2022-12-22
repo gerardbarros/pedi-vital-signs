660886cd725511afdb0a46875f8a11c8583d5c8c
Gerard <barros.gc@gmail.com>
2022-12-22 01:17:29 -0600

    updated object names for vitalSigns

--- server.js
+++ server.js
@@ -8,64 +8,64 @@ app.use (cors())
 
 const vitalSigns = {
     "neonate": {
-        "heart rate": "110-170",
-        "respiratory rate": "40-70",
+        "heartRate": "110-170",
+        "respiratoryRate": "40-70",
         "systolic": "55-75",
         "diastolic": "65-85",
     },
 
     "newborn": {
-        "heart rate": "110-160",
-        "respiratory rate": "35-55",
+        "heartRate": "110-160",
+        "respiratoryRate": "35-55",
         "systolic": "65-85",
         "diastolic": "45-55",
     },
     
     "infant": {
-        "heart rate": "110-160",
-        "respiratory rate": "30-45",
+        "heartRate": "110-160",
+        "respiratoryRate": "30-45",
         "systolic": "70-90",
         "diastolic": "50-65",
     },
 
     "toddler": {
-        "heart rate": "90-160",
-        "respiratory rate": "22-38",
+        "heartRate": "90-160",
+        "respiratoryRate": "22-38",
         "systolic": "80-100",
         "diastolic": "55-65",
     },
 
     "preSchool": {
-        "heart rate": "80-150",
-        "respiratory rate": "22-30",
+        "heartRate": "80-150",
+        "respiratoryRate": "22-30",
         "systolic": "90-105",
         "diastolic": "60-75",
     },
 
     "schoolAge": {
-        "heart rate": "70-120",
-        "respiratory rate": "18-25",
+        "heartRate": "70-120",
+        "respiratoryRate": "18-25",
         "systolic": "97-115",
         "diastolic": "57-76",
     },
     
     "preTeen": {
-        "heart rate": "60-110",
-        "respiratory rate": "16-22",
+        "heartRate": "60-110",
+        "respiratoryRate": "16-22",
         "systolic": "100-120",
         "diastolic": "60-75",
     },
 
     "adolescent": {
-        "heart rate": "60-100",
-        "respiratory rate": "12-20",
+        "heartRate": "60-100",
+        "respiratoryRate": "12-20",
         "systolic": "110-135",
         "diastolic": "60-75",
     },
 
     "age group not found": {
-        "heart rate": "unknown",
-        "respiratory rate": "unknown",
+        "heartRate": "unknown",
+        "respiratoryRate": "unknown",
         "systolic": "unknown",
         "diastolic": "unknown",
     }
