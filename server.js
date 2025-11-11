6d7ddb9c6e2349305c6e15745cf811ff5e0c6955
Gerard Barros <barros.gc@gmail.com>
2025-11-10 22:22:33 -0600

    Update parameter names.

--- server.js
+++ server.js
@@ -8,43 +8,43 @@ app.use (cors())
 
 const vitalSigns = {
     'neonate': {
-        'pulse': '70-170',
-        'respirations':'30-50',
+        'heartRate': '70-170',
+        'respiratoryRate':'30-50',
         'systolic': '50-70',
         'diastolic': '20-40'
     },
 
     'infant': {
-        'pulse': '80-160',
-        'respirations':'26-40',
+        'heartRate': '80-160',
+        'respiratoryRate':'26-40',
         'systolic': '60-90',
         'diastolic': '20-60'
     },
     
     'toddler': {
-        'pulse': '80-130',
-        'respirations':'20-30',
+        'heartRate': '80-130',
+        'respiratoryRate':'20-30',
         'systolic': '75-100',
         'diastolic': '50-70'
     },
 
     'preschool': {
-        'pulse': '80-110',
-        'respirations':'20-30',
+        'heartRate': '80-110',
+        'respiratoryRate':'20-30',
         'systolic': '80-110',
         'diastolic': '50-80'
     },
 
     'schoolage': {
-        'pulse': '75-100',
-        'respirations':'20-24',
+        'heartRate': '75-100',
+        'respiratoryRate':'20-24',
         'systolic': '82-120',
         'diastolic': '50-80'
     },
 
     'adolescent': {
-        'pulse': '60-90',
-        'respirations':'16-20',
+        'heartRate': '60-90',
+        'respiratoryRate':'16-20',
         'systolic': '85-130',
         'diastolic': '55-80'
     },
